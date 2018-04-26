5b9d2e25a86109e6d51546b74dd30243bae03398
tprlab <dev@tprlab.com>
2018-04-26 02:15:54 -0400

    removed unnecessary files

--- mock.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-@app.route("/name")
-def name():
-    return jsonify({"name":"mock"}), 200
-
-app.run(debug=True, host="0.0.0.0")
